9f99b41ef80b963bda96d1c587d4758323704dac
Saff-Buraq-Dev <gharbi.safwen@hotmail.com>
2024-06-03 15:47:22 -0400

    deploy s3 bucket

--- lib/github-oidc-stack.ts
+++ lib/github-oidc-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { v4 as uuidv4 } from 'uuid';
 
 export class GithubOidcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Generate a UUID
+    const uuid = uuidv4();
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GithubOidcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new Bucket(this, 'MyFirstBucket', {
+      bucketName: `my-dummy-bucket-${uuid}`, // Globally unique Bucket name with UUID
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
   }
 }
